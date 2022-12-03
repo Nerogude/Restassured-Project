04564707f7641fd992e281a445543e20f9de5b32
nero <ogudenero@gmail.com>
2022-12-03 17:19:00 +0000

    fixes

--- src/test/java/day1/HTTPRequests.java
+++ src/test/java/day1/HTTPRequests.java
@@ -4,11 +4,13 @@ import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
+import java.util.HashMap;
+
 import org.testng.annotations.Test;
 
 public class HTTPRequests {
 	
-	@Test
+	//@Test(priority=1)
 	void getUsers() {
 		
 		given()
@@ -19,6 +21,24 @@ public class HTTPRequests {
 		.then()
 			.statusCode(200)
 			.body("page",equalTo(2))
+			.log().all();
+	}
+	@Test
+	void createUser() {
+		
+		HashMap data = new HashMap();
+		data.put("name", "nero");
+		data.put("job", "qa analyst");
+		
+		given()
+			.contentType("application/json")
+			.body(data)
+				
+		.when()
+			.post("https://reqres.in/api/users")
+			
+		.then()
+			.statusCode(201)
 			.log().all();
 	}
 
